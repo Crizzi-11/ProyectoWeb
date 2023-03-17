a7bf4374767f6ec9e573de6d1779ff014eab3d3e
Crizzi-11 <cmarin90677@ufide.ac.cr>
2023-03-17 00:06:23 -0600

    Primer Avance con la coneccion de la base de datos al netbeans Primeras 2 tablas

---
+++ Proyecto/src/main/java/com/Proyecto/proyecto/service/IPaisService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Proyecto.proyecto.service;
+
+import com.Proyecto.proyecto.entity.Pais;
+import java.util.List;
+
+/**
+ *
+ * @author Cristofer
+ */
+
+
+public interface IPaisService {
+    public List<Pais> listCountry();
+    
+}
