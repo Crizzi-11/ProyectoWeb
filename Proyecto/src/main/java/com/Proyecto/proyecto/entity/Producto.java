e886f941f3308148c93593d1100fbdaf7a4e3f8f
Richie023 <ricardoulloaaraya0203@gmail.com>
2023-04-16 17:38:35 -0600

    Se agrego 2 tablas más,se edito el moda.html y se agrego el productoController

---
+++ Proyecto/src/main/java/com/Proyecto/proyecto/entity/Producto.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Proyecto.proyecto.entity;
+
+import com.Proyecto.proyecto.entity.Categoria;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+/**
+ *
+ * @author ricar_t6uqtj3
+ */
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+      @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+    private String nombreArticulo;
+    private String descripcion;
+    private double precio;
+ 
+   
+    
+       @ManyToOne
+    @JoinColumn(name = "categorias_id")
+     private Categoria categoria;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombreArticulo
+     */
+    public String getNombreArticulo() {
+        return nombreArticulo;
+    }
+
+    /**
+     * @param nombreArticulo the nombreArticulo to set
+     */
+    public void setNombreArticulo(String nombreArticulo) {
+        this.nombreArticulo = nombreArticulo;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+   
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+       
+
+}
